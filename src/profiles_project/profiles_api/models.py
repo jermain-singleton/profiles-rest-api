06a37a47a0c411a738688bfc286a66b45caf6ec1
596932 <jermainsingleton@c02wd0jmhtdg.users.bah.com>
2018-05-03 10:56:40 -0400

    added a custom userprofile model, manager and the migraton for the DB

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class UserProfileManager(BaseUserManager):
+    """ Helps Django work with our custom user model. """
+
+    def create_user(self,email,name,password=None):
+        """creates a new user profile objects"""
+        if not email:
+            raise ValueError('Users must have a email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_supersuser(self,email,name,password):
+        """Create and saves a new superuser with given details."""
+
+        user = self.create_user(email,name,password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    """Represents a "user profile" inside our system. """
+
+    email = models.EmailField(max_length=250,unique=True)
+    name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """ Used to get a users full name. """
+
+        return self.name
+
+    def get_short_name(self):
+        """ Used to get a users short name. """
+
+        return self.name
+
+
+    def __str__(self):
+        """Return a string of self fields"""
+
+        return self.email
