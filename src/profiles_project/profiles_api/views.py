9a73c9c9df69a85ae660ef96dcb8c3282cf63a28
596932 <jermainsingleton@c02wd0jmhtdg.users.bah.com>
2018-05-04 11:00:45 -0400

    Added Post, Put,Patch and Delete to our API view

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,20 +1,54 @@
 from django.shortcuts import render
 
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-class HelloApiView(APIView):
-    """Test API View"""
+from . import serializers
+from rest_framework import status
 
-    def get(self,request,format=None):
-        """Return a list of api view features"""
+# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View."""
+
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features."""
 
         an_apiview = [
-            'Uses HTTP methjods as function (get, post, patch, put, delete)',
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
             'It is similar to a traditional Django view',
             'Gives you the most control over your logic',
-            'Is mapped manually to URLs',
+            'Is mapped manually to URLs'
         ]
 
-        return Response({'message':'Hello!', 'an_apiview': an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}!'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk=None):
+        """Handles updating an object."""
+
+
+        return Response({'method': 'put'})
+
+    def patch(self, request,pk=None):
+        """Patch request, only updates fields provided in the request."""
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request,pk=None):
+        """"Delete and object."""
+        return Response({'method': 'delete'})
